6dfefc573a55ecd5caf4eb02e0d223e3f19f5159
MikelArias <115534269+MikelArias@users.noreply.github.com>
2024-06-09 16:27:58 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,32 +1,40 @@
 import os
 
+#Opción para actualizar la máquina
 def actualizar_maquina():
     os.system('sudo apt update && sudo apt upgrade')
     print("Actualizado.")
 
+#Opción para acceder a la página web Los40Asirpales
 def acceder_enlace_1():
     os.system('xdg-open http://83.60.148.121/')
     print("Abriendo...")
-
+    
+#Opción para acceder a la radio para monitorearla
 def acceder_enlace_2():
     os.system('xdg-open http://83.60.148.121:8000/')
     print("Abriendo...")
 
+#Opción para acceder a gestionar la página WordPress
 def acceder_enlace_3():
     os.system('xdg-open http://83.60.148.121/wp-admin/')
     print("Abriendo...")
-
+    
+#Opción para editar la playlist de Reggaeton
 def playlist1():
     os.system('sudo nano /etc/ices2/ices-playlist.xml')
     print("Abriendo...")
 
+#Opción para editar la playlist de Rock
 def playlist2():
     os.system('sudo nano /etc/ices2/ices-playlist2.xml')
     print("Abriendo...")
 
+#Opción para iniciar la radio
 def radio():
     os.system('sudo ices2 /etc/ices2/ices-playlist.xml & sudo ices2 /etc/ices2/ices-playlist2.xml &')
 
+#Menú que contiene las opciones
 def mostrar_menu():
     print("1. Actualizar la máquina")
     print("2. Acceder a la página web Los40Asirpales")
@@ -37,6 +45,7 @@ def mostrar_menu():
     print("7. Iniciar radio")
     print("0. Salir")
 
+#Bucle para que funcione el menú
 while True:
     mostrar_menu()
     opcion = input("Selecciona una opción: ")
